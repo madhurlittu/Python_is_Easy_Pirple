0ed5ad5584d96e14a381701f28121bcd80d72fab
madhurlittu <46478099+madhurlittu@users.noreply.github.com>
2019-01-09 01:57:46 +0530

    Add files via upload

---
+++ Homewor #2 Functions.py
@@ -0,0 +1,18 @@
+Composer = 'A R Rehman'
+Year = 2009
+Duration_in_Mins = 5.53
+def composer():
+    print(Composer)
+    
+def year():
+    print (Year)
+   
+def duration_in_Mins():
+    print(Duration_in_Mins)
+    
+
+composer()
+
+year()
+
+duration_in_Mins()
---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+homework Assignment #1: Variables
+
+Attributes of my Fav Song
+'''
+
+SongName = 'Newyork Nagaram'
+Composer = 'A R Rehman'
+Singer = 'A R Rehman'
+Album = 'Nuvvu Nenu Prema'
+Duration_in_Mins = 5.53
+Year = 2009
+
+print('My Favorite song is: ',end ='')
+print(SongName)
+print(Composer)
+print(Singer)
+print(Album)
+print(Duration_in_Mins)
+print(Year)
